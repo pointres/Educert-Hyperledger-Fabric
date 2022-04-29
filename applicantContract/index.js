72eec54c819148d81b4d5388ec4d85f690596e21
Yash Deorah <55012588+yrd123@users.noreply.github.com>
2022-04-30 01:07:34 +0530

    Delete applicantContract directory

--- applicantContract/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const applicantContract = require('./lib/primaryContract');
-
-module.exports.applicantContract = applicantContract;
-module.exports.contracts = [applicantContract];
